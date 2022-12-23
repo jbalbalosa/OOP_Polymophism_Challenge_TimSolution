ff4da4fb0f346c116256bd18292ad56bca2e4ee8
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-23 15:37:02 +0900

    commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,50 @@
+package com.timbuchalka;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine(){
+        return  "Car -> startEngine()";
+    }
+
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+
+    public String brake(){
+        return "Car -> brake()";
+    }
+
+}
+
+class Mitsubishi extends Car{
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
